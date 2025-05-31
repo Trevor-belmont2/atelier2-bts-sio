d2fbc92dd841100441d312c0e9f5f24d040a273b
Trevor-belmont2 <elkhamlichisaad22@gmail.com>
2025-05-31 20:52:21 +0200

    Delete model directory

--- model/serviceModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-
-function getAllServices() {
-    global $pdo;
-    $sql = "SELECT * FROM service";
-    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-}
-?>
