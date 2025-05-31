f0869b774a9ce7d11c92ee121136fe3c850cdb53
Saad El Khamlichi <elkhamlichisaad22@gmai.com>
2025-05-31 20:25:19 +0200

    Déplacement des fichiers dans le dossier 05_Application

--- 05_Application/index.php
+++ 05_Application/index.php
@@ -1,5 +1,11 @@
 <?php
-// Fichier d'entrée principal de l'application
-// Appelle le contrôleur du personnel
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+
+if (!isset($_GET['action'])) {
+    header('Location: index.php?action=list');
+    exit;
+}
 
 require_once(__DIR__ . '/controller/personnelController.php');
